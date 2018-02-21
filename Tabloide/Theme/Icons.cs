e43c66df75bd27d1cddb6203f579b8f9ab39d94b
Marlon Santos Constante <marlon.constante@gmail.com>
2018-02-21 17:15:35 -0300

    Ícones do app

---
+++ Tabloide/Theme/Icons.cs
@@ -0,0 +1,41 @@
+﻿namespace Tabloide.Theme
+{
+	public static class Icons
+	{
+		public const string Menu = "\ue5d2";
+
+		public const string Search = "\ue8b6";
+
+		public const string Close = "\ue5cd";
+
+		public const string Assignment = "\ue85d";
+
+		public const string AddBox = "\ue146";
+
+		public const string RemoveBox = "\ue909";
+
+		public const string ArrowBack = "\ue5c4";
+
+		public const string ArrowRight = "\ue315";
+
+		public const string Star = "\ue838";
+
+		public const string StarBorder = "\ue83a";
+
+		public const string ShoppingBasket = "\ue8cb";
+
+		public const string Favorite = "\ue87d";
+
+		public const string Place = "\ue55f";
+
+		public const string AccessTime = "\ue192";
+
+		public const string Phone = "\ue0cd";
+
+		public const string Email = "\ue0be";
+
+		public const string Store = "\ue8d1";
+
+		public const string WifiOff = "\ue1da";
+	}
+}
