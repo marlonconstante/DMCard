f493f0a466c2d7ba4792bafe6b8e5e8d8870f6f0
Marlon Santos Constante <marlon.constante@gmail.com>
2018-02-23 12:02:28 -0300

    Controle DividerBar

---
+++ Tabloide/Controls/DividerBar.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace Tabloide.Controls
+{
+	public partial class DividerBar : ContentView
+	{
+		public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(DividerBar), default(string));
+
+		public string Title {
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+
+		public DividerBar()
+		{
+			InitializeComponent();
+		}
+	}
+}
