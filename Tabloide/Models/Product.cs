c3b6fad91cc29de00f11cc98949bc6bccfad7db3
Marlon Santos Constante <marlon.constante@gmail.com>
2018-02-26 16:24:44 -0300

    Modelo de Produto

---
+++ Tabloide/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Tabloide.Models
+{
+	public class Product
+	{
+		public long Code {
+			get;
+			set;
+		}
+
+		public string Name {
+			get;
+			set;
+		}
+
+		public string Description {
+			get;
+			set;
+		}
+
+		public double NormalPrice {
+			get;
+			set;
+		}
+
+		public double PromotionPrice {
+			get;
+			set;
+		}
+
+		public double CardPrice {
+			get;
+			set;
+		}
+
+		public string ImageSource {
+			get;
+			set;
+		}
+
+		public DateTimeOffset ExpirationDate {
+			get;
+			set;
+		}
+	}
+}
