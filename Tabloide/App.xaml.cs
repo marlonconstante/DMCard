b2736be65c0f95ff83014480632ce74aaeb0ad1e
Marlon Santos Constante <marlon.constante@gmail.com>
2018-02-26 17:26:30 -0300

    Registrando serviço do mostruário

--- Tabloide/App.xaml.cs
+++ Tabloide/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Prism;
 using Prism.Ioc;
 using Prism.Unity;
+using Tabloide.Services.Showcase;
 using Xamarin.Forms;
 
 namespace Tabloide
@@ -19,6 +20,8 @@ namespace Tabloide
 
 		protected override void RegisterTypes(IContainerRegistry containerRegistry)
 		{
+			containerRegistry.Register<IShowcaseService, ShowcaseService>();
+
 			containerRegistry.RegisterForNavigation<NavigationPage>();
 			containerRegistry.RegisterForNavigation<MainPage>();
 		}
