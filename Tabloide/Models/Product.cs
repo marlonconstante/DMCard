f2dacd8dfd43d2d6327b3c093fb544b507d01cdd
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-02 09:03:47 -0300

    Formatação do modelo de produto

--- Tabloide/Models/Product.cs
+++ Tabloide/Models/Product.cs
@@ -4,44 +4,20 @@ namespace Tabloide.Models
 {
 	public class Product
 	{
-		public long Code {
-			get;
-			set;
-		}
+		public long Code { get; set; }
 
-		public string Name {
-			get;
-			set;
-		}
+		public string Name { get; set; }
 
-		public string Description {
-			get;
-			set;
-		}
+		public string Description { get; set; }
 
-		public double NormalPrice {
-			get;
-			set;
-		}
+		public double NormalPrice { get; set; }
 
-		public double PromotionPrice {
-			get;
-			set;
-		}
+		public double PromotionPrice { get; set; }
 
-		public double CardPrice {
-			get;
-			set;
-		}
+		public double CardPrice { get; set; }
 
-		public string ImageSource {
-			get;
-			set;
-		}
+		public string ImageSource { get; set; }
 
-		public DateTimeOffset ExpirationDate {
-			get;
-			set;
-		}
+		public DateTimeOffset ExpirationDate { get; set; }
 	}
 }
