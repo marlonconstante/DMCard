d8a20eba1aae281fc4540c890fe7eabbaf496c20
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-02 10:24:47 -0300

    App Settings

---
+++ Tabloide/Configurations/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Tabloide.Configurations
+{
+	public class AppSettings
+	{
+		public string ApplicationName { get; set; }
+
+		public string StoreName { get; set; }
+
+		public string StoreCardName { get; set; }
+
+		public string Language { get; set; }
+
+		public string MoneyMask { get; set; }
+
+		public string DateMask { get; set; }
+	}
+}
