3e3376aa15a84d30623457fb784d3d9bdc9c5985
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-02 09:46:57 -0300

    Product Thumbnail Template

---
+++ Tabloide/Views/Templates/ProductThumbnailTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Tabloide.Views.Templates
+{
+	public partial class ProductThumbnailTemplate : ContentView
+	{
+		public ProductThumbnailTemplate()
+		{
+			InitializeComponent();
+		}
+	}
+}
