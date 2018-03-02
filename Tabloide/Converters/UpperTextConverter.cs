93902381d223ca8c0f3dd6aa90b1da36bd81f309
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-02 16:48:12 -0300

    Upper Text Converter

---
+++ Tabloide/Converters/UpperTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Tabloide.Converters
+{
+	public class UpperTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value?.ToString().ToUpper(culture);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
