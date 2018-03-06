4cd634346dbdbb31ed078ef7bfb6045595e4b4cc
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-06 15:30:32 -0300

    Screen Size

---
+++ Tabloide/Platform/Display/ScreenSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabloide.Platform.Display
+{
+	public class ScreenSize
+	{
+		public double FullWidth { get; set; }
+
+		public double FullHeight { get; set; }
+
+		public double HalfWidth => FullWidth * 0.5d;
+
+		public double HalfHeight => FullHeight * 0.5d;
+
+		public double QuarterWidth => FullWidth * 0.25d;
+
+		public double QuarterHeight => FullHeight * 0.25d;
+
+		public ScreenSize(double width, double height)
+		{
+			FullWidth = Math.Min(width, height);
+			FullHeight = Math.Max(width, height);
+		}
+	}
+}
