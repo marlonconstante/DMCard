a82bec6af7a626a0a5f1198eb33e1257f7b6a08e
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-06 11:49:57 -0300

    Not Null Boolean Converter

---
+++ Tabloide/Converters/NotNullBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Tabloide.Converters
+{
+	public class NotNullBooleanConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value != null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
