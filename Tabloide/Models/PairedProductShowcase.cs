93be632cf4ada11fdb0d91026d2a856df8294e85
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-06 11:52:06 -0300

    Paired Product Showcase

---
+++ Tabloide/Models/PairedProductShowcase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmHelpers;
+
+namespace Tabloide.Models
+{
+	public class PairedProductShowcase : Grouping<string, Tuple<Product, Product>>
+	{
+		public PairedProductShowcase(ProductShowcase showcase) : base(showcase.Category, GetPairedProducts(showcase))
+		{
+		}
+
+		static IEnumerable<Tuple<Product, Product>> GetPairedProducts(ProductShowcase showcase)
+		{
+			using (var iterator = showcase.GetEnumerator()) {
+				while (iterator.MoveNext()) {
+					var first = iterator.Current;
+					var second = iterator.MoveNext() ? iterator.Current : default(Product);
+
+					yield return Tuple.Create(first, second);
+				}
+			}
+		}
+	}
+}
