165641d41e43b0056538c9cb2708227db3785c24
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-07 08:40:35 -0300

    Has Data Converter

--- Tabloide/Converters/HasDataConverter.cs
+++ Tabloide/Converters/HasDataConverter.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Globalization;
 using Xamarin.Forms;
 
 namespace Tabloide.Converters
 {
-	public class NotNullBooleanConverter : IValueConverter
+	public class HasDataConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
+			if (value is string) {
+				return !string.IsNullOrWhiteSpace(value as string);
+			}
+
+			if (value is ICollection) {
+				return (value as ICollection).Count > 0;
+			}
+
 			return value != null;
 		}
 
