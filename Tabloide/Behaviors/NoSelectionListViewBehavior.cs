9126160726d45651cc1f20b3b06a0206be7f46de
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-08 11:32:25 -0300

    No Selection ListView Behavior

---
+++ Tabloide/Behaviors/NoSelectionListViewBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace Tabloide.Behaviors
+{
+	public class NoSelectionListViewBehavior : Behavior<ListView>
+	{
+		protected override void OnAttachedTo(ListView bindable)
+		{
+			base.OnAttachedTo(bindable);
+
+			bindable.ItemSelected += OnItemSelected;
+		}
+
+		protected override void OnDetachingFrom(ListView bindable)
+		{
+			base.OnDetachingFrom(bindable);
+
+			bindable.ItemSelected -= OnItemSelected;
+		}
+
+		void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			(sender as ListView).SelectedItem = null;
+		}
+	}
+}
