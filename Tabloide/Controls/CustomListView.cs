a3c0285b13bab8c36bf4f6278cabd2548f3834a7
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-08 11:34:02 -0300

    Custom ListView

---
+++ Tabloide/Controls/CustomListView.cs
@@ -0,0 +1,13 @@
+﻿using Tabloide.Behaviors;
+using Xamarin.Forms;
+
+namespace Tabloide.Controls
+{
+	public class CustomListView : ListView
+	{
+		public CustomListView()
+		{
+			Behaviors.Add(new NoSelectionListViewBehavior());
+		}
+	}
+}
