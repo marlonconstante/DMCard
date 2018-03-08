e3594db3bbd124cee6a279541737f4c0eaf38e4b
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-08 11:58:11 -0300

    Custom ListView Renderer (Android)

---
+++ Droid/Renderers/CustomListViewRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using Tabloide.Controls;
+using Tabloide.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AGColor = Android.Graphics.Color;
+using ARColor = Android.Resource.Color;
+
+[assembly: ExportRenderer(typeof(CustomListView), typeof(CustomListViewRenderer))]
+namespace Tabloide.Droid.Renderers
+{
+	public class CustomListViewRenderer : ListViewRenderer
+	{
+		public CustomListViewRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null) {
+				Control.SetSelector(ARColor.Transparent);
+				Control.CacheColorHint = AGColor.Transparent;
+			}
+		}
+	}
+}
