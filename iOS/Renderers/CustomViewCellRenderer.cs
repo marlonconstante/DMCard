2a32363581cfc5dbdb0e864990c904f6e047a382
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-08 12:00:53 -0300

    Custom View Cell Renderer (iOS)

---
+++ iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Tabloide.Controls;
+using Tabloide.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomViewCell), typeof(CustomViewCellRenderer))]
+namespace Tabloide.iOS.Renderers
+{
+	public class CustomViewCellRenderer : ViewCellRenderer
+	{
+		public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+		{
+			var cell = base.GetCell(item, reusableCell, tv);
+
+			if (cell != null) {
+				cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+			}
+
+			return cell;
+		}
+	}
+}
