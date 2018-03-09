12c8fb497b7af9e5573e1491d98c95cab07aa24d
Marlon Santos Constante <marlon.constante@gmail.com>
2018-03-09 16:55:49 -0300

    ProductThumbnailTemplate: Tap Command

--- Tabloide/Views/Templates/ProductThumbnailTemplate.xaml.cs
+++ Tabloide/Views/Templates/ProductThumbnailTemplate.xaml.cs
@@ -1,9 +1,17 @@
-﻿using Xamarin.Forms;
+﻿using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Tabloide.Views.Templates
 {
 	public partial class ProductThumbnailTemplate : ContentView
 	{
+		public static readonly BindableProperty TapCommandProperty = BindableProperty.Create(nameof(TapCommand), typeof(ICommand), typeof(ProductThumbnailTemplate), default(ICommand));
+
+		public ICommand TapCommand {
+			get { return (ICommand)GetValue(TapCommandProperty); }
+			set { SetValue(TapCommandProperty, value); }
+		}
+
 		public ProductThumbnailTemplate()
 		{
 			InitializeComponent();
